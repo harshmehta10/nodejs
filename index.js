ef3a5195e78ed5c127b2eead9c016154a1460083
Harsh Mehta <viveksinghmehta.smr@gmail.com>
2025-08-05 12:21:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+const os = require("os");
+
+////////////////////////
+// File
+// const textIn = fs.readFileSync("./starter/txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// const textOut =
+//   "This is all we know about the avocado :${textIn}.\nCreated on " + Date.now();
+// fs.writeFileSync("./starter/txt/output.txt", textOut);
+// console.log("File written successfully");
+// const hello = "Hello World";
+// console.log(hello);
+
+////////////////////////
+// Server
+// console.log(os.cpus().length);
+// fs.readFile(`${__dirname}/starter/dev-data/data.json`, "utf-8", (err, data) => {
+//   const productData = JSON.parse(data);
+// });
+
+// const server = http.createServer((req, res) => {
+//   const pathname = req.url;
+
+//   if (pathname === "/" || pathname === "/overview") {
+//     res.end("This is the OVERVIEW");
+//   } else if (pathname === "/product") {
+//     res.end("This is the PRODUCT");
+//   } else if (pathname === "/api") {
+//     fs.readFile(
+//       `${__dirname}/starter/dev-data/data.json`,
+//       "utf-8",
+//       (err, data) => {
+//         const productData = JSON.parse(data);
+//         res.writeHead(200, {
+//           "Content-type": "application/json",
+//         });
+//         res.end(data);
+//       }
+//     );
+//   } else {
+//     res.writeHead(404, {
+//       "Content-type": "text/html",
+//       "my-own-header": "hello-world",
+//     });
+//     res.end("<h1>Page not found</h1>");
+//   }
+// });
+
+// server.listen(8000, "127.0.0.1", () => {
+//   console.log("Listening to requests on port 8000");
+// });
+
+const server = http.createServer((req, res) => {
+  const log = `${Date.now()}: ${req.url} New Req Received\n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    res.end("Hello from the server!");
+  });
+});
+
+server.listen(8000, () => console.log("Server Started"));
